6c1336e867999975131b7edcf1b8a303e3a2d287
PiyushAgrawal123 <piyush.agarwal@knoldus.com>
2022-10-09 19:49:55 +0530

    css update

--- index.js
+++ index.js
@@ -1,29 +1,6 @@
-var isValid = function(s) {
 
-    let obj = {
-        ')': '(',
-        '}': '{',
-        ']': '['
-    }
-    let result = [];
-    let check = true;
-     for(let i =0 ; i < s.length; i++){
 
-        if( check && result.length > 0){
-
-        }
-         
-        if(result.length == 0 && i ==0){
-            result.push(s[i]);
-        }else if( obj[s[i]] == result[result.length-1]){
-            result.pop();
-        } else {
-            result.push(s[i]);
-        }
-     }
- 
-     return  s.length- result.length;
-     
- };
-
- console.log(isValid('(]'))
+ function showCard(event){
+    event.target.classList.add('active')
+    console.log(event.target)
+}
